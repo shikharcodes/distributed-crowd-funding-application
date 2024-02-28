d077589817f1996d20ec1596fe3f994e09ea747c
Shikhar Saxena <shikhar06jan@gmail.com>
2024-02-28 09:07:01 +0530

    developed the createCampaign function
    Signed-off-by: Shikhar Saxena <shikhar06jan@gmail.com>

--- web3/contracts/FundRaiser.sol
+++ web3/contracts/FundRaiser.sol
@@ -19,7 +19,25 @@ contract FundRaiser {
     uint256 public numberOfCampaigns = 0;
 
     // to create a new campaign
-    function createCampaign() {}
+    // returns ID of the specific campaign
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // to check if everything is ok
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
 
     // to donate to a specific campaign
     function donateToCampaign() {}
