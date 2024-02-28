0e89419cb84f4e92d5cfc0f5a7be498395ae9bcb
Shikhar Saxena <shikhar06jan@gmail.com>
2024-02-28 09:18:08 +0530

    developed the donateToCampaign function
    Signed-off-by: Shikhar Saxena <shikhar06jan@gmail.com>

--- web3/contracts/FundRaiser.sol
+++ web3/contracts/FundRaiser.sol
@@ -40,7 +40,21 @@ contract FundRaiser {
     }
 
     // to donate to a specific campaign
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        // let us know if the transaction has been sent or not
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        } 
+    }
 
     // gives us a list of all the people who have donated to the campaign
     function getDoantors() {}
