b5648e6791133e3a6b1f89aab9c3996313e25b27
Shikhar Saxena <shikhar06jan@gmail.com>
2024-03-07 11:10:24 +0530

    developed and added the navbar component
    Signed-off-by: Shikhar Saxena <shikhar06jan@gmail.com>

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { logo, sun } from '../assets';
 import { navlinks } from '../constants';
 
+// Icon component
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
