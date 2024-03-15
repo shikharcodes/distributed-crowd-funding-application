a162c6e47dede32c1327e6c94c398f41526a835f
Shikhar Saxena <shikhar06jan@gmail.com>
2024-03-15 20:52:15 +0530

    smart contract deployment related changes
    Signed-off-by: Shikhar Saxena <shikhar06jan@gmail.com>

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
-contract FundRaiser {
+contract CrowdFunding {
     struct Campaign {
         address owner;
         string title;
