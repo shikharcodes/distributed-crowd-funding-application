6f2540e2b5e040f3a617dc531c1fc86923399e03
Shikhar Saxena <shikhar06jan@gmail.com>
2024-04-22 13:06:06 +0530

    developed and added the home page and the user profile page
    Signed-off-by: Shikhar Saxena <shikhar06jan@gmail.com>

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -39,6 +39,31 @@ export const StateContextProvider = ({ children }) => {
             console.log("contract call failure", error)
         }
     }
+
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+
+        const parsedCampaigns = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }));
+
+        return parsedCampaigns;
+    }
+
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+        return filteredCampaigns;
+    }
     
     return (
         <StateContext.Provider
@@ -49,6 +74,8 @@ export const StateContextProvider = ({ children }) => {
                 // disconnect,
                 createCampaign: publishCampaign,
                 // walletInstance
+                getCampaigns,
+                getUserCampaigns,
             }}
         >
             {children}
